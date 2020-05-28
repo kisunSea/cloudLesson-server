f9f93fb90fbfffea3b19fc7b351023e21f38d562
zk <zhangkai@clerware.com>
2020-05-28 09:29:06 +0800

    临时提交-处理异常

--- logic/resource_upload.py
+++ logic/resource_upload.py
@@ -4,11 +4,15 @@ Web 资源上传+处理逻辑
 import re
 import string
 import json
+import os
 
 import xlrd
 
 from teaching_helper import gdata
 from wx_client import models
+from teaching_helper import glog
+
+_logger = glog.get_logger(__name__)
 
 
 def fetch_path_and_name(file_item):
@@ -52,6 +56,15 @@ def resolving_excel(file_path) -> list:
         return [{'key': i[1], 'content': i[0]}
                 for i in zip(filter(lambda __c: __c != '', choices), predefine_name)]
 
+    def __get_difficulty_level(difficult: str):
+        if '简单' in difficult:
+            return gdata.DIFFICULTY_SIMPLE
+        if '一般' in difficult:
+            return gdata.DIFFICULTY_MIDDLE
+        if '困难' in difficult:
+            return gdata.DIFFICULTY_COMPLEX
+        return gdata.DIFFICULTY_UNKNOWN
+
     workbook = xlrd.open_workbook(file_path)
     worksheet = workbook.sheet_by_index(0)
 
@@ -64,7 +77,7 @@ def resolving_excel(file_path) -> list:
             'p_type': row_info[1],  # 类型
             'stem': row_info[2],  # 题干
             'score': int(row_info[3]) if row_info[3] else 0,  # 分值
-            'difficult': row_info[4],  # 难度
+            'difficult': __get_difficulty_level(row_info[4]),  # 难度
             'related_knowledge': re.split(r'[，,]', row_info[5]),  # 关联知识点
             'analysis': '无解析' if not row_info[6] else row_info[6],  # 解析
             'answer': row_info[7],  # 答案
@@ -86,7 +99,8 @@ class ResourceStorage(object):
         """试卷资源类型
         """
         file_path, f_name = fetch_path_and_name(file_item)
-        exam_data = resolving_excel(file_path)
+        exam_data = resolving_excel(os.path.join(gdata.BASE_RESOURCE_PATH, file_path))
+        _logger.info('examination information: {}'.format(exam_data))
 
         # 试卷相关
         exam = models.Examination.objects.create(title=f_name, lesson=lesson_obj)
@@ -97,7 +111,7 @@ class ResourceStorage(object):
                                                content=_q['stem'],
                                                related_point=_q['related_knowledge'],
                                                related_section=_q['chapter'],
-                                               difficulty=_q['difficulty'],
+                                               difficulty=_q['difficult'],
                                                score=_q['score'],
                                                answer='{}|（{}）'.format(_q['answer'], _q['analysis']),
                                                questions=json.dumps(_q['choices']))
